14cb74875b054b5751b3b5d996e889a47cc46f41
Patrice Thimothee <thimpat@gmail.com>
2022-08-30 20:53:56 +0100

    DEMO: Update demos

--- demo/demo.mjs
+++ demo/demo.mjs
@@ -4,19 +4,18 @@
  * @see [./demo/demo.cjs]{@link ./demo/demo.cjs}
  * 
  **/
-import fs  from "fs";
 import {showHelp}  from "../esm/index.mjs";
 
 
 (async function ()
 {
-    let content = fs.readFileSync("./demo/random-text.md", {encoding: "utf-8"})
-    await showHelp(content, {
+    await showHelp("", {
         windowTitle: "❔" + " Help ",
         topText: "Press CTRL + C or Q to Quit | Page Down to scroll down | Any key to next line",
         topTextBg: "",
         topTextReversed: true,
         colorify: true,
-        markdown: true
+        markdown: true,
+        filepath: "./demo/random-text.md"
     });
 }())
