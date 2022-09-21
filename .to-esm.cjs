de336e13a04c942de035fe49de8c5875de5b9c10
Patrice Thimothee <thimpat@gmail.com>
2022-09-21 20:17:17 +0100

    BUILD: Update modules

--- .to-esm.cjs
+++ .to-esm.cjs
@@ -1,23 +1,4 @@
-const directiveContent = `
-import terminalSize from 'term-size';
-const getTerminalHeight = () =>
-{
-    return terminalSize().rows
-};
-
-const getTerminalWidth = () =>
-{
-    return terminalSize().cols
-};
-`
-
 module.exports = {
-    replaceEnd    : [
-        {
-            search : "// import terminalSize from 'term-size';",
-            replace: ""
-        }
-    ],
     replaceModules: {
         "term-size": {
             cjs: {
